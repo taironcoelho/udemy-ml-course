748b140efd7543f7fa94bb051c81fa19b93852aa
i869598 <tairon.coelho@sap.com>
2018-08-16 23:16:21 -0300

    Add knn and random forest algorithms

---
+++ knn/census.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+base = pd.read_csv('./datasets/census.csv')
+
+forecasts = base.iloc[:,0:14].values
+
+classes = base.iloc[:,14].values
+
+#Encode string labels in num values
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+label_encoder_forecasts = LabelEncoder()
+forecasts[:,1] = label_encoder_forecasts.fit_transform(forecasts[:,1])
+forecasts[:,3] = label_encoder_forecasts.fit_transform(forecasts[:,3])
+forecasts[:,5] = label_encoder_forecasts.fit_transform(forecasts[:,5])
+forecasts[:,6] = label_encoder_forecasts.fit_transform(forecasts[:,6])
+forecasts[:,7] = label_encoder_forecasts.fit_transform(forecasts[:,7])
+forecasts[:,8] = label_encoder_forecasts.fit_transform(forecasts[:,8])
+forecasts[:,9] = label_encoder_forecasts.fit_transform(forecasts[:,9])
+forecasts[:,13] = label_encoder_forecasts.fit_transform(forecasts[:,13])
+
+# This combinated with standard scaler decrease the precision
+# one_hot_encoder = OneHotEncoder(categorical_features=[1,3,5,6,7,8,9,13])
+# forecasts = one_hot_encoder.fit_transform(forecasts).toarray()
+
+label_encoder_class = LabelEncoder()
+classes = label_encoder_class.fit_transform(classes)
+
+#when using knn algorithms is necessary standardisation or normalization
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+forecasts = scaler.fit_transform(forecasts)
+
+#Divide database in training data and test data
+from sklearn.cross_validation import train_test_split
+forecasts_training, forecasts_testing, classes_training, classes_testing = train_test_split(forecasts, classes, test_size= 0.25, random_state=0)
+
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
+classifier.fit(forecasts_training, classes_training)
+
+predicts = classifier.predict(forecasts_testing)
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+precision = accuracy_score(classes_testing, predicts)
+
+matrix = confusion_matrix(classes_testing, predicts)# Visualize how many records were correct per class
+print(precision)
---
+++ random-forest/credit_data.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+base = pd.read_csv('./datasets/credit-data.csv') #load a file
+base.loc[base.age < 0, 'age'] = base['age'][base.age > 0].mean() #replace all negative ages to ages mean
+
+base.loc[pd.isnull(base['age'])] #find out on base all indexes with null age
+
+forecasts = base.iloc[:, 1:4].values #forecasts variable will receive the columns 1,2 and 3 of base. The ":" means we want all lines.
+classes = base.iloc[:, 4].values
+
+#Using sklearn to localize all missing_values and replace using a strategy
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
+imputer = imputer.fit(forecasts[:,1:4])
+forecasts[:,1:4] = imputer.transform(forecasts[:,1:4])
+
+#when using knn algorithms is necessary standardisation or normalization
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+forecasts = scaler.fit_transform(forecasts)
+
+#Divide database in training data and test data
+from sklearn.cross_validation import train_test_split
+forecasts_training, forecasts_testing, classes_training, classes_testing = train_test_split(forecasts, classes, test_size= 0.25, random_state=0)
+
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=40, criterion='entropy', random_state=0)#n_estimators eq num of trees in forest
+classifier.fit(forecasts_training, classes_training)
+
+predicts = classifier.predict(forecasts_testing)
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+precision = accuracy_score(classes_testing, predicts)
+
+matrix = confusion_matrix(classes_testing, predicts)# Visualize how many records were correct per class
+print(precision)
+
